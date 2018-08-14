81b17452f2e0a1209bd4596d75b2d494c5a85272
Im-Adarsh <40120624+Im-Adarsh@users.noreply.github.com>
2018-08-14 17:13:52 +0530

    Added WeightedNode POJO

---
+++ gp/WeightedNode.java
@@ -0,0 +1,29 @@
+package com.dsj.gp;
+
+public class WeightedNode {
+	String vertexId;
+	int edgeWeight;
+
+	public WeightedNode(String vertexId, int numOfVertices) {
+		this.vertexId = vertexId;
+		this.edgeWeight = numOfVertices;
+
+	}
+
+	public String getVertexId() {
+		return vertexId;
+	}
+
+	public void setVertexId(String vertexId) {
+		this.vertexId = vertexId;
+	}
+
+	public int getEdgeWeight() {
+		return edgeWeight;
+	}
+
+	public void setEdgeWeight(int edgeWeight) {
+		this.edgeWeight = edgeWeight;
+	}
+
+}
